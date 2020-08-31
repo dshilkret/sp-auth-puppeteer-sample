289ded8276fce488bd94c5bf8db29557c0a87203
David Shilkret <dshilkret@gmail.com>
2020-08-31 09:36:28 -0500

    Update getTitle.ts

--- src/cases/getTitle.ts
+++ src/cases/getTitle.ts
@@ -4,4 +4,5 @@ export const run = async (page: Page, siteUrl: string): Promise<void> => {
   // Print page title
   const pageTitle = await page.title();
   console.log('Page title:', pageTitle);
+  
 };
