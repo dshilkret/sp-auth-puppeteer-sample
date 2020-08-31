16ee61572a60e83d8914aa1cca7f07ec84d0752d
David Shilkret <dshilkret@gmail.com>
2020-08-31 09:39:27 -0500

    Update getTitle.ts

--- src/cases/getTitle.ts
+++ src/cases/getTitle.ts
@@ -3,6 +3,6 @@ import { Page } from 'puppeteer';
 export const run = async (page: Page, siteUrl: string): Promise<void> => {
   // Print page title
   const pageTitle = await page.title();
-  console.log('Page title:', pageTitle);
+ 
   
 };
